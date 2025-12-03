39d40c4741fef4432efc523d3c38c8fbccebd8e4
TedVinnitchouk <t.vinnitchouk@gmail.com>
2025-12-03 16:34:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ window.addEventListener("scroll", () => {
   const scroll = window.scrollY;
   const hue = scroll % 360;
 
-  // update the main name color
-  document.getElementById("name").style.color = `hsl(${hue}, 100%, 60%)`;
+  const color = `hsl(${hue}, 100%, 60%)`;
+
+  // Update main name color
+  document.getElementById("name").style.color = color;
+
+  // Update CSS variable for hover color
+  document.documentElement.style.setProperty("--accent-color", color);
 });
